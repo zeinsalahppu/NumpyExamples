c2e87d06c9128a90ffd76d3253e7912401bacc50
zeinsalahppu <zeinsalah@gmail.com>
2020-04-11 20:02:06 +0300

    2d and 3d arrays

--- numpy_examples.py
+++ numpy_examples.py
@@ -6,14 +6,34 @@ simple NumPy Examples
 import numpy as np
 
 
-a_np = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(a_np)
+# a_np = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# print(a_np)
+#
+# lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a_np = np.array(lst)
+# print(a_np)
+#
+# lst = [i for i in range(10)]
+# a_np = np.array(lst)
+# print(a_np)
 
-lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-a_np = np.array(lst)
-print(a_np)
+lst_2d = [[0, 1, 2, 3],
+         [4, 5, 6, 7],
+         [8, 9, 10, 11]]
+a_2d = np.array(lst_2d)
 
-lst = [i for i in range(10)]
-a_np = np.array(lst)
-print(a_np)
+print(a_2d)
+print(a_2d.shape)
+print(a_2d.ndim)
+print(a_2d.dtype)
+print(a_2d.itemsize)
+print(a_2d.size)
+print(type(a_2d))
 
+
+lst_3d = [[[0,  1], [2,  3]],
+          [[4,  5], [6,  7]],
+          [[8,  9], [10,11]] ]
+a_3d = np.array(lst_3d)
+print(a_3d)
+print(a_3d.shape)
