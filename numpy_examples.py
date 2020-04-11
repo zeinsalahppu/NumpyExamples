486ea8a04902fa7ac7f670809e7ee16b7022ba00
zeinsalahppu <zeinsalah@gmail.com>
2020-04-11 19:29:04 +0300

    very basic creation of arrays

---
+++ numpy_examples.py
@@ -0,0 +1,19 @@
+"""
+simple NumPy Examples
+
+"""
+
+import numpy as np
+
+
+a_np = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(a_np)
+
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+a_np = np.array(lst)
+print(a_np)
+
+lst = [i for i in range(10)]
+a_np = np.array(lst)
+print(a_np)
+
