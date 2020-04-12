9a91137db7a1364e7bf69a58556e779180285d37
zeinsalahppu <zeinsalah@gmail.com>
2020-04-12 16:25:18 +0300

    numerical integration using midpoint method

--- numpy_examples.py
+++ numpy_examples.py
@@ -5,15 +5,24 @@ simple NumPy Examples
 
 import numpy as np
 
+def f(x):
+    return x**2
 
-a_np = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(a_np)
 
-lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-a_np = np.array(lst)
-print(a_np)
+def integrate(f, a, b, N):
+    dx = (b-a)/N
+    x = np.linspace(a+dx/2, b-dx/2, N)
+    fx = f(x)
+    area = np.sum(fx)*dx
+    return area
 
-lst = [i for i in range(10)]
-a_np = np.array(lst)
-print(a_np)
+print(integrate(np.sin, 0, np.pi/2, 100))
+print(integrate(np.sin, 0, np.pi*2, 100))
+
+print(integrate(f, 0, 3, 100))
+
+a = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(f(a))
+
 
